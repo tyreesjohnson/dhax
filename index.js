55968b799bbe4ff798c4983ffd68dd4a7a2a1478
Tyrees Johnson <rainrevoltv3@gmail.com>
2020-06-08 14:27:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -161,9 +161,3 @@ app.get('/logout',(req,res)=>{
     res.redirect('/');
 });
 // END OF LOGOUT
-
-app.use('/', (req,res) => {
-    res.status(404).send('<h1>404 Page Not Found!</h1>');
-});
-
-app.listen(3000, () => console.log("Server is Running..."));
